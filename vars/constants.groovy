6820119be9a4e62be07643554f45a25a2577c629
Darshan8050 <Darshan.J@sapiens.com>
2024-12-26 07:16:45 +0530

    added artifactory setting.xml id

--- vars/constants.groovy
+++ vars/constants.groovy
@@ -3,6 +3,8 @@ import groovy.transform.Field
 
 @Field String gitUrl = "https://github.com/devops5014/sample-webapp.git"
 @Field String password = "github-pass"
+@Field String artifactorysetting = "d583ab88-2c36-49aa-b582-49c2b6cc4d05"
+
 
 
 
